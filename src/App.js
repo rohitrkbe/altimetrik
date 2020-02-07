72976e81ec02418b8d3dc876861141ee4a7ed134
rohitrkbe <rohitrkbe@WORKSPACE.ALTIMETRIK.COM>
2020-02-07 04:59:11 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,132 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const dataBus = require('./data.json');
+
+const SearchBar = props => {
+  const {destination, source, travelDate, returnDate} = props.propsData;
+  return(
+    <div>
+      <p>Search Bus</p>
+      <div>
+          <TextField id="source" label="Enter Source" variant="outlined" onChange={(event)=>props.handleSourceChange(event)} />
+          <TextField id="destination" label="Enter Destination" variant="outlined" onChange={(event)=>props.handleDestinationChange(event)} />
+          <label for="start">Travel Date:</label>
+          <input type="date" id="start" name="trip-start" value={travelDate?travelDate:null} min="2020-01-01" max="2020-12-31" onChange={(event)=>props.handleTravelDate(event)}></input>
+          <label for="return">Return Date:</label>
+          <input type="date" id="return" name="trip-return" value={returnDate?returnDate:'select'} min="2020-01-01" max="2020-12-31" onChange={(event)=>props.handleReturnDate(event)}></input>
+          <Button variant="contained" color="primary" onClick={()=>props.handleSubmitSerach()} >Search Bus</Button>
+      </div>
     </div>
-  );
+  )
+}
+
+const BusList = ( props ) => {
+  console.log(props);
+  return(
+    <div>
+      <p>Bus Details</p>
+      <p>Total Result : {dataBus.travelBusList.length} </p>
+      {
+        dataBus.travelBusList.map((item, index) => (
+          <div key={index+'1q1q1q'} >
+            <p>{index+1}</p>
+            <p>{item.BusOperatorName} - {item.BusNo}</p>
+            <p>Departure - {item.arrivalTime} , Arrival- {item.departureTime}</p>
+            <p>Ticket Amount - {item.price}</p>
+            <Button variant="contained" color="secondary" onClick={()=>props.handleBookBus(item)} >Select Bus</Button>
+          </div>
+        ))
+      }
+    </div>
+  )
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataLoaded: false,
+      source: '',
+      destination: '',
+      travelDate:'',
+      returnDate:'',
+      ShowBusList: false,
+    };
+  }
+
+  componentDidMount(){
+    this.setState({ dataLoaded: true })
+  }
+
+  handleSourceChange = event => {
+    this.setState({source: event.target.value});
+  }
+
+  handleDestinationChange = event => {
+    this.setState({source: event.target.value});
+  }
+
+  handleTravelDate = event => {
+    this.setState({travelDate: event.target.value});
+  }
+
+  handleReturnDate = event => {
+    this.setState({returnDate: event.target.value});
+  }
+
+  handleSubmitSerach = () =>{
+    // do validation
+    // make api call
+    this.setState({ dataLoaded: false })
+    setTimeout(()=>{ this.setState({ ShowBusList: true, dataLoaded: true }) },2000);
+  }
+
+  handleBookBus = (busData) => {
+    console.log('selected busData', busData);
+  }
+
+  render(){
+    const { ShowBusList, dataLoaded } = this.state;
+    console.log('%cState Variable','color:blue', this.state);
+    if ( !dataLoaded ){
+      return (
+        <p>Loading...</p>
+      )
+    }
+    return (
+      <div className="App">
+        <div className='HeaderText'>
+          <h3>Bus Booking Application</h3>
+        </div>
+        {
+          !ShowBusList
+          ?
+            <SearchBar propsData= {this.state} 
+              handleSourceChange= {(event) =>this.handleSourceChange(event)}
+              handleDestinationChange= {(event) =>this.handleDestinationChange(event)} 
+              handleTravelDate= {(event) =>this.handleTravelDate(event)}
+              handleReturnDate= {(event) =>this.handleReturnDate(event)}
+              handleSubmitSerach= {() =>this.handleSubmitSerach()}
+            />
+          :
+            <BusList handleBookBus={(busData)=>this.handleBookBus(busData)} />
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: 200,
+    },
+  },
+}));
